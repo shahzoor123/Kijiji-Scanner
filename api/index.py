cbe2251707612b9d6b12b3422040baad9ed41418
shahzor khan <shahzoorkhan321@gmail.com>
2023-12-06 22:55:57 +0500

    deployment

--- api/index.py
+++ api/index.py
@@ -1,3 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from email.mime.text import MIMEText
+import schedule
+import time
+import os
+from twilio.rest import Client
 from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
@@ -7,4 +15,10 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/plain')
         self.end_headers()
         self.wfile.write('Hello, world!'.encode('utf-8'))
+        
+        self.x = 100
+        
+        for i in range(self.x):
+            print(i)
+        
         return
