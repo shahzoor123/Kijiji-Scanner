a466f706075e1aece4106f061cc2c085309d5557
shahzor khan <shahzoorkhan321@gmail.com>
2023-12-06 20:29:26 +0500

    email notification is changed to sms notification + updated the code where it save new ad in the file + sms formate is updated all done to deploy

--- email_notification.py
+++ email_notification.py
@@ -1,31 +1,31 @@
-# import smtplib
-# from email.mime.text import MIMEText
+import smtplib
+from email.mime.text import MIMEText
 
 
-# def send_email(ad_data, url):
+def send_email(ad_data, url):
     
 
-#     # Set up your email configuration
-#     sender_email = 'temporary98778@gmail.com'
-#     receiver_email = 'juiceofprogramming@gmail.com'
-#     subject = 'Ad Listing'
+    # Set up your email configuration
+    sender_email = 'temporary98778@gmail.com'
+    receiver_email = 'juiceofprogramming@gmail.com'
+    subject = 'Ad Listing'
 
-#     # Compose the email content
-#     body = f"Title:There is a ad listed today title is: {ad_data}  please check on the following link {url}"
+    # Compose the email content
+    body = f"Title:There is a ad listed today title is: {ad_data}  please check on the following link {url}"
     
-#     print(body)
+    print(body)
     
-#     message = MIMEText(body)
-#     message['Subject'] = subject
-#     message['From'] = sender_email
-#     message['To'] = receiver_email
+    message = MIMEText(body)
+    message['Subject'] = subject
+    message['From'] = sender_email
+    message['To'] = receiver_email
 
-#     # Connect to the SMTP server and send the email
-#     server = smtplib.SMTP('smtp.gmail.com', 587)
-#     server.starttls()
-#     server.login(sender_email, 'meml pxij bged qaee')
-#     server.sendmail(sender_email, receiver_email, message.as_string())
-#     print('Email is sended')
+    # Connect to the SMTP server and send the email
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender_email, 'meml pxij bged qaee')
+    server.sendmail(sender_email, receiver_email, message.as_string())
+    print('Email is sended')
         
         
         
--- scraper.py
+++ scraper.py
@@ -13,84 +13,15 @@ from twilio.rest import Client
 script_dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(script_dir)
 
-def scrape_data(url):
-    # Make a request to the website
-    response = requests.get(url)
-    
-    print(response.url)
-    
-    ad_list = []
-    
-    if response.status_code == 200:
-        # Parse HTML content
-        soup = BeautifulSoup(response.text, 'html.parser')
 
-        ads = soup.find_all('a', class_='sc-2e07e5ea-0 gpxjNR')
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
         
-        img = soup.find_all('img', class_='sc-92b4b92a-0 kFFtpF sc-388271ae-2 fugJbm')
-        
-        for ad in ads:
-            ad_list.append(ad.text)
-            print(ad.text)
-            
-        for i in img:
-            print(i)    
-            
-        print("f1")    
-
-    return ad_list
-
-
-
-
-
-def send_whatsapp_msg(ad_data, url):
-    
-    account_sid = "AC96d42f46854b5cf18cd371b2a556c7cb"
-    account_auth = "a90f655fbb8cffcdb381f084a4d90fac"
-    twillio_number = "+14155238886"
-    receipiant_number = "+923138409209"
-    
-    client = Client(account_sid, account_auth)
-    
-    message = client.messages.create(
-        body = 'Hello',
-        from_=twillio_number,
-        to=receipiant_number
-    )
-    
-    print('sended')
-    
-
-    # Set up your email configuration
-    sender_number = '03362131109'
-    receiver_number = '03138409209'
-    subject = 'Ad Listing'
-
-    # Compose the number content
-    body = f"Title:There is a ad listed today title is: {ad_data}  please check on the following link {url}"
-    
-    print(body)
-    
-    message = MIMEText(body)
-    message['Subject'] = subject
-    message['From'] = sender_number
-    message['To'] = receiver_number
-
-    # Connect to the SMTP server and send the number
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(sender_number, 'meml pxij bged qaee')
-    server.sendmail(sender_number, receiver_number, message.as_string())
-    print('Whatsapp msg is sended')
-        
-
-
-        
+# Function to check for new ads
 def checking():
     url_to_scrape_actual_data = 'https://www.kijiji.ca/b-cars-trucks/city-of-toronto/2000__2016/c174l1700273a68?kilometers=100000__300000&for-sale-by=ownr&radius=176.0&price=700__2500&address=434+Bay+St.%2C+Toronto%2C+ON+M5G+1P5%2C+Canada&ll=43.6534662%2C-79.38323009999999'
-    
-    # Check for new ads
+
+    # Scrape new ads
     new_ads = scrape_data(url_to_scrape_actual_data)
 
     # Update the seen ads file
@@ -108,19 +39,74 @@ def checking():
         unseen_ads = new_ads_set - seen_ads
 
         if unseen_ads:
-            # Send email notification
-            
-            
-            # send_email(list(unseen_ads), url_to_scrape_actual_data)
-
-            # Update the seen ads file
+            # Print and add new ads to the seen ads file
+            print("New ads found:")
+            for ad in unseen_ads:
+                print(ad)
             with open(seen_ads_file, 'a') as file:
-                file.write('\n'.join(new_ads_set))
+                file.write('\n'.join(unseen_ads))
+                file.write('\n')
+                Send_SMS(url_to_scrape_actual_data, unseen_ads)
+
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+def scrape_data(url):
+    # Make a request to the website
+    response = requests.get(url)
+    
+    print(response.url)
+    
+    ad_list = []
+    
+    if response.status_code == 200:
+        # Parse HTML content
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+      
+        ads = soup.find_all('a', class_='sc-388271ae-0 hLAAKW')
+        
+        
+        for ad in ads:
+            ad_list.append(ad.text)
+            print(ad.text)
+
+
+    return ad_list
+
+
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 
 
 
+def Send_SMS(url, ad_data):
+    
+    print("i am in send sms function " , url, ad_data)
+    
+    account_sid = "AC96d42f46854b5cf18cd371b2a556c7cb"
+    account_token = "8bb0e7ff782e663eb2f14a2fd9ac336d"
+    twillio_number = "+16184214328"
+    receipiant_number = "+6478820415"
 
+    client = Client(account_sid, account_token)
+
+    message = client.messages.create(
+        body = f'Hi Sameer We Found New Ads Post of your interest Here it is Title : {ad_data} Link : {url}',
+        from_=twillio_number,
+        to=receipiant_number
+    )
+
+    print('sended')
+    
+   
+
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+    
 # Schedule the checking function to run every 5 minutes
 schedule.every(.1).minutes.do(checking)
 
